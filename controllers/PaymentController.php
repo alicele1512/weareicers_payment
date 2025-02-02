6e2c64fd38a6e8e4f9f0ef2cf2981adfe3693321
alicele1512 <45648174+alicele1512@users.noreply.github.com>
2025-02-02 21:52:59 +0700

    Update PaymentController.php

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -6,25 +6,25 @@ include '../vendor/weareicers_payment/gateways/CardGateway.php';
 include '../vendor/weareicers_payment/gateways/StripeGateway.php';
 
 class PaymentController {
-    private $conn;
+    private $connect;
 
-    public function __construct($conn) {
-        $this->conn = $conn;
+    public function __construct($connect) {
+        $this->connect = $connect;
     }
 
     public function processPayment($payment_method, $user_id, $amount, $trans_id) {
         switch ($payment_method) {
             case 'paypal':
-                $gateway = new PayPalGateway($this->conn);
+                $gateway = new PayPalGateway($this->connect);
                 return $gateway->processPayment($user_id, $amount, $trans_id);
             case 'momo':
-                $gateway = new MoMoGateway($this->conn);
+                $gateway = new MoMoGateway($this->connect);
                 return $gateway->processPayment($user_id, $amount, $trans_id);
             case 'card':
-                $gateway = new CardGateway($this->conn);
+                $gateway = new CardGateway($this->connect);
                 return $gateway->processPayment($user_id, $amount, $trans_id);
             case 'stripe':
-                $gateway = new StripeGateway($this->conn);
+                $gateway = new StripeGateway($this->connect);
                 return $gateway->processPayment($user_id, $amount, $trans_id);
             default:
                 return false;
